f74e803cf82712d3ea673a0ba13620385fc14209
Emily Fernandez <emilyfern121@gmail.com>
2021-03-27 22:54:45 -0400

    added generate password function

--- script.js
+++ script.js
@@ -1,89 +1,89 @@
-// A series of prompts with password criteria options display.
-
-// Prompt 1: "How many characters would you like your password to have?" Must be at least 8 characters and no more than 128 characters.
-
-// Prompt 2: Click OK to confirm including special characters.
-
-// Prompt 3: Click OK to confirm numeric characters.
-
-// Prompt 4: Click OK to confirm lowercase characters.
-
-// Prompt 5: Click OK to confirm uppercase characters.
-
-// Once all prompts are answered, the password should be generated based on the users input. 
-
-// Once the password is generated and the button is clicked, the password is written on the page
-
 // Assignment Code
 
 var generateBtn = document.querySelector("#generate");
 
-var lcase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// Generate password function
 
-var ucase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+function generatePassword () {
 
-var num = [1,2,3,4,5,6,7,8,9];
+  var password = [];
 
-var char = ["!","@","#","$","%","^","&","*","~"];
+  var lcase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var allOptions = [];
+  var ucase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var passwordLength=prompt("How many characters would you like your password to have?");
+  var num = [1,2,3,4,5,6,7,8,9];
 
-  if (passwordLength >= 8) {
+  var char = ["!","@","#","$","%","^","&","*","~"];
+
+  var allOptions = [];
+
+  var passwordLength=prompt("How many characters would you like your password to have?");
+
+    if (passwordLength >= 8) {
     console.log(passwordLength);
     allOptions.push("");
-  }
+    }
   
-var specialCharacter=confirm("Click OK to confirm including special characters.")
+  var specialCharacter=confirm("Click OK to confirm including special characters.")
   
-  if (specialCharacter) {
+    if (specialCharacter) {
     console.log(specialCharacter);
-    allOptions.push("!","@","#","$","%","^","&","*","~");
+    allOptions=allOptions.concat(char);
+    password.push(getRandomElementFromArray(char));
+    }
   
-  }
+  var numeric=confirm("Click OK to confirm numeric characters.");
   
-var numeric=confirm("Click OK to confirm numeric characters.");
-  
-  if (numeric) {
+    if (numeric) {
     console.log(numeric);
-    allOptions.push(1,2,3,4,5,6,7,8,9);
-  }
+    allOptions=allOptions.concat(num);
+    password.push(getRandomElementFromArray(num));
+    }
   
-var lowercase=confirm("Click OK to confirm lowercase characters.");
+  var lowercase=confirm("Click OK to confirm lowercase characters.");
   
-  if (lowercase) {
+    if (lowercase) {
     console.log(lowercase);
-    allOptions.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-  }
+    allOptions=allOptions.concat(lcase);
+    password.push(getRandomElementFromArray(lcase));
+    }
   
-var uppercase=confirm("Click OK to confirm uppercase characters.");
+  var uppercase=confirm("Click OK to confirm uppercase characters.");
   
-  if (uppercase) {
+    if (uppercase) {
     console.log(uppercase);
-    allOptions.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-  }
+    allOptions=allOptions.concat(ucase);
+    password.push(getRandomElementFromArray(ucase));
+    }
 
+    for (var i=password.length; i < passwordLength; i++) {
+    password.push(getRandomElementFromArray(allOptions));
+    }
+    return password.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  passwordText.value = password;
 }
 
-// function to generate random number 
-function generateRandomNumber(maxValue) {
-  var randomNumber=Math.random();
-  var result=Math.floor(randomNumber * maxValue);
-  return result;
-}
+//function generate random number
+ function generateRandomNumber(maxValue) {
+   var randomNumber=Math.random();
+
+   var result=Math.floor(randomNumber * maxValue);
+   return result;
+ }
 
 // function to generate random array
 function getRandomElementFromArray(array) {
   var randomPosition=generateRandomNumber(array.length);
   return array[randomPosition];
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
