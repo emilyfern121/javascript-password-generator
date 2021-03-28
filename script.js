a786b6a26483872f86b1361df7a34389a038b0da
Emily Fernandez <emilyfern121@gmail.com>
2021-03-27 20:42:03 -0400

    updated booleans and added new arrays

--- script.js
+++ script.js
@@ -18,35 +18,51 @@
 
 var generateBtn = document.querySelector("#generate");
 
+var lcase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var ucase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var num = [1,2,3,4,5,6,7,8,9];
+
+var char = ["!","@","#","$","%","^","&","*","~"];
+
+var allOptions = [];
+
 var passwordLength=prompt("How many characters would you like your password to have?");
 
-if (passwordLength <= 10) {
-  console.log(passwordLength);
-}
-
-var specialCharacter=confirm("Click OK to confirm including special characters.")
-
-if (specialCharacter) {
-  console.log(specialCharacter);
-}
-
-var numeric=confirm("Click OK to confirm numeric characters.");
-
-if (numeric) {
-  console.log(numeric);
-}
-
-var lowercase=confirm("Click OK to confirm lowercase characters.");
-
-if (lowercase) {
-  console.log(lowercase);
-}
-
-var uppercase=confirm("Click OK to confirm uppercase characters.");
-
-if (uppercase) {
-  console.log(uppercase);
-}
+  if (passwordLength <= 10) {
+    console.log(passwordLength);
+    allOptions.push("");
+  }
+  
+  var specialCharacter=confirm("Click OK to confirm including special characters.")
+  
+  if (specialCharacter) {
+    console.log(specialCharacter);
+    allOptions.push("!","@","#","$","%","^","&","*","~");
+  
+  }
+  
+  var numeric=confirm("Click OK to confirm numeric characters.");
+  
+  if (numeric) {
+    console.log(numeric);
+    allOptions.push(1,2,3,4,5,6,7,8,9);
+  }
+  
+  var lowercase=confirm("Click OK to confirm lowercase characters.");
+  
+  if (lowercase) {
+    console.log(lowercase);
+    allOptions.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+  }
+  
+  var uppercase=confirm("Click OK to confirm uppercase characters.");
+  
+  if (uppercase) {
+    console.log(uppercase);
+    allOptions.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+  }
 
 
 // Write password to the #password input
